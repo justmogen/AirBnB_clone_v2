cc686c4d8d46f3dcd4ce38a8d6384dad801b1755
justmogen <justmogen@gmail.com>
2023-07-22 07:49:26 +0300

    adding python route

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -3,35 +3,36 @@
 Your web application must be listening on 0.0.0.0, port 5000
 """
 
-from flask import Flask
+from flask import Flask, escape
 
 app = Flask("__name__")
 
 
 @app.route('/', strict_slashes=False)
 def hello():
-    """Return a given string"""
+    """Return the given string"""
     return ("Hello HBNB!")
 
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
-    """Returns a given string"""
+    """Returns the given string"""
     return ("HBNB")
 
 
 @app.route("/c/<text>", strict_slashes=False)
-def cText(text):
-    """display C followed by the value of the text variable"""
-    return "C {}".format(text.replace("_", " "))
+def c_route(text):
+    """display C followed by the value of the text"""
+    text = escape(text).replace('_', ' ')
+    return f'C {text}'
 
 
 @app.route('/python', strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
-def pythonText(text="is cool"):
-    """display Python followed by the value of the text variable"""
-    return "Python {}".format(text.replace("_", " "))
+def python_route(text="is cool"):
+    """display Python followed by the value of the text"""
+    return "Python {}".format(text.replace('_', ' '))
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=None)
+    app.run(host="0.0.0.0", port=5000)
