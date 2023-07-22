18a20a4a6db84dda45a275876336c70042768831
justmogen <justmogen@gmail.com>
2023-07-22 07:28:26 +0300

    Can display two values depending on the route passed

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,29 +1,22 @@
 #!/usr/bin/python3
-""" Write a script that starts a Flask web application:
-Your web application must be listening on 0.0.0.0, port 5000
+""" Starts a Flask web application listening on 0.0.0.0, port 5000
 """
 
 from flask import Flask
 
 app = Flask("__name__")
 
-
+# route to display "Hello HBNB"
 @app.route('/', strict_slashes=False)
-def hello():
-    """Return a given string"""
+def hello_hbnb():
+    """Return Hello HBNB"""
     return ("Hello HBNB!")
 
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
-    """Returns a given string"""
+    """Returns HBNB"""
     return ("HBNB")
-
-
-@app.route("/c/<text>", strict_slashes=False)
-def cText(text):
-    """display C followed by the value of the text variable"""
-    return "C {}".format(text.replace("_", " "))
 
 
 if __name__ == "__main__":
